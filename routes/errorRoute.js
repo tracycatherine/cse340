41a07695ef594b17e8001b01892cdcd94b6e6eb2
tracycatherine <tracynalubwama5@gmail.com>
2025-06-17 16:14:38 +0300

    week5

--- routes/errorRoute.js
+++ routes/errorRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = new express.Router();
-const errorController = require("../controllers/errorcontroller");
+const errorController = require("../controllers/errorController");
 
 // Intentional error route
 router.get("/trigger-error", errorController.triggerError);
